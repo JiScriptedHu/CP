37534328e3f5c7b90804272790cd23a6532adf80
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2026-06-23 23:58:42 +0530

    23 June 2026

---
+++ codeforces/practice/2101-2200/2182A.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/contest/2182/problem/A
+
+// 23-06-2026
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        if (s.find("2026") != string::npos || s.find("2025") == string::npos) {
+            cout << 0 << endl;
+        }
+        else {
+            cout << 1 << endl;
+        }
+    }
+}
