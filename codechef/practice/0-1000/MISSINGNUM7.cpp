79d60a49689b74974db1bac4c7a3a01370262f4f
JiScriptedHu <jayeshkumarnag1627@gmail.com>
2026-09-04 23:54:08 +0530

    04 September 2026

---
+++ codechef/practice/0-1000/MISSINGNUM7.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/MISSINGNUM7
+
+// 04-09-2026
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int s;
+	cin >> s;
+	
+	cout << 10 - s << endl;
+}
